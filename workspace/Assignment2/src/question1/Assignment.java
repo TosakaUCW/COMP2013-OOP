f8ec4b7192245145301bca3c21b71353b0e6666a
TosakaUCW <tosakaucw0@gmail.com>
2025-03-10 00:38:38 +0800

    finished Assignment2

--- workspace/Assignment2/src/question1/Assignment.java
+++ workspace/Assignment2/src/question1/Assignment.java
@@ -1,6 +1,43 @@
 package question1;
 
 public class Assignment {
+    // Attributes
+    private Code myCode;
+    private int score;
+    private boolean submitted;
+    private String name;
+    
+    // Constructor
+    public Assignment(Code myCode, boolean submitted, String name) {
+        this.myCode = myCode;
+        this.score = 0;
+        this.submitted = submitted;
+        this.name = name;
+    }
+    
+    public void submit() {
+        this.submitted = true;
+    }
+    
+    public boolean isSubmitted() {
+        return this.submitted;
+    }
+    
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+    public int getScore() {
+        return this.score;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public Code getCode() {
+        return this.myCode;
+    }
 
     // Tests
     public static void testAssignment() {
