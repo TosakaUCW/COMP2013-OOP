b4e9042acbf7bee1ab4c95200007d08f961e1f2c
TosakaUCW <tosakaucw0@gmail.com>
2025-03-11 16:18:53 +0800

    feat: Add code for Lab5 questions 1-4

---
+++ workspace/Lab5/src/question1/Cat.java
@@ -0,0 +1,18 @@
+package question1;
+
+public class Cat {
+
+	public static void testCat() {
+
+		System.out.println("Test constructor");
+		Cat c = new Cat("Meow", 2.0);
+		System.out.println("name: " + (c.getName() == "Meow"));
+		System.out.println("weight: " + (c.getWeight() == 2.0));
+
+		System.out.println("\nTest feed");
+		c.feed();
+		// The name is still the same but the weight increased by 1.0:
+		System.out.println("name: " + (c.getName() == "Meow"));
+		System.out.println("weight: " + (c.getWeight() == 3.0));
+	}
+}
---
+++ workspace/Lab5/src/question4/Student.java
@@ -0,0 +1,26 @@
+package question4;
+
+public class Student {
+	
+	public static void testStudent() {
+		System.out.println ("Test constructor");
+		Cat c = new Cat("Meow", 2.0);
+		Student s0 = new Student("Philippe", c);
+		System.out.println("name: " + (s0.getName() == "Philippe"));
+		System.out.println("pet: " + (s0.getPet() == c));
+
+		System.out.println ("\nTest s1's pet");
+		// Creating a new student with a dog as pet:
+		Dog d = new Dog("Woof", 2.0);
+		Student s1 = new Student("Mr. Li", d);
+		System.out.println("name: " + (s1.getName() == "Mr. Li"));
+		System.out.println("pet: " + (s1.getPet() == d));
+
+		System.out.println ("\nTest s2's pet");
+		// We can now also use an Animal object as a pet:
+		Animal a = new Animal("Blob", 5.0);
+		Student s2 = new Student("Ms. Chen", a);
+		System.out.println("name: " + (s2.getName() == "Ms. Chen"));
+		System.out.println("pet: " + (s2.getPet() == a));
+	}
+}
