3bb72cde9c3cd1eaef4800bd65ac1284fe04b6b8
TosakaUCW <tosakaucw0@gmail.com>
2025-03-25 16:22:38 +0800

    feat: Add Lab7

---
+++ workspace/Lab7/src/question3/Animal.java
@@ -0,0 +1,10 @@
+package question3;
+
+// The Animal class must be abstract because its two methods getLegs
+// and canFly (below) are abstract.
+public abstract class Animal {
+	
+	public static void testAnimal() {
+		// Animal a = new Animal("Unknown"); // This does not work!
+	}
+}
---
+++ workspace/Lab7/src/question3/Bird.java
@@ -0,0 +1,11 @@
+package question3;
+
+// The class Bird derives from the class Animal.
+// The class must be abstract because it does not have any code for
+// the canFly method (see below).
+public abstract class Bird extends Animal {
+	
+	public static void testBird() {
+		// Bird b = new Bird("Twitter", 3); // This does not work!
+	}
+}
