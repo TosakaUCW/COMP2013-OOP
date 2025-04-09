45bb61112f230968c2b35969599226828eb4a149
TosakaUCW <tosakaucw0@gmail.com>
2025-04-09 09:54:37 +0800

    chore: Refactor BadRadiusException, CannotResizeException, Circle, Dot, Rectangle, Square, and Shape classes

--- workspace/Lab8/src/question8/BadRadiusException.java
+++ workspace/Lab8/src/question8/BadRadiusException.java
@@ -1,5 +1,13 @@
 package question8;
 
 public class BadRadiusException extends Exception{
-   
+    public BadRadiusException(String message) {
+        super(message);
+    }
+    // The BadRadiusException class is not abstract (it has code for all methods) so
+    // we can test it.
+    public static void testBadRadiusException() {
+        BadRadiusException ex = new BadRadiusException("Bad radius!");
+        System.out.println(ex.getMessage().equals("Bad radius!"));
+    }
 }
