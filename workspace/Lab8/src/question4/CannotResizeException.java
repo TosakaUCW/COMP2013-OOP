49ea0618ec952d7216273263da4c716b3e14ce2a
TosakaUCW <tosakaucw0@gmail.com>
2025-04-09 09:25:48 +0800

    chore: Refactor CannotResizeException, Circle, Dot, and Shape classes

--- workspace/Lab8/src/question4/CannotResizeException.java
+++ workspace/Lab8/src/question4/CannotResizeException.java
@@ -2,5 +2,14 @@ package question4;
 
 // The CannotResizeException class derives from the Exception class.
 public class CannotResizeException extends Exception {
-	
+    // The constructor initializes the message.
+    public CannotResizeException(String message) {
+        super(message);
+    }
+    // The CannotResizeException class is not abstract (it has code for all methods) so
+    // we can test it.
+    public static void testCannotResizeException() {
+        CannotResizeException ex = new CannotResizeException("Cannot resize a dot!");
+        System.out.println(ex.getMessage().equals("Cannot resize a dot!"));
+    }
 }
